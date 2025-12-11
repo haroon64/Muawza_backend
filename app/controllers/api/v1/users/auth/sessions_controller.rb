2a3220676948604a861d1366374aa2c8ea989bc1
Haroon Siddique <haroonsiddique97@gmail.com>
2025-12-11 15:33:12 +0500

    Update session api resolve signup bug

--- app/controllers/api/v1/users/auth/sessions_controller.rb
+++ app/controllers/api/v1/users/auth/sessions_controller.rb
@@ -4,30 +4,28 @@ class Api::V1::Users::Auth::SessionsController < Devise::SessionsController
 
 
   def create
-
     if sign_in_params[:user].nil?
       return render json: {
-        status: { code: 400, message: 'User parameters are required.' }
+        status: { code: 400, message: "User parameters are required." }
       }, status: :bad_request
     end
 
     login_param = sign_in_params[:user][:email].presence || sign_in_params[:user][:phone_number].presence
-    role_param = sign_in_params[:user][:role]
 
     if login_param.blank? || sign_in_params[:user][:password].blank?
       return render json: {
-        status: { code: 400, message: 'Email/phone and password are required.' }
+        status: { code: 400, message: "Email/phone and password are required." }
       }, status: :bad_request
     end
 
-    user = User.find_by(email: login_param, role: role_param) ||
+    user = User.find_by(email: login_param) ||
            User.find_by(phone_number: login_param, role: role_param)
 
     if user&.valid_password?(sign_in_params[:user][:password])
       token = JwtService.encode(user_id: user.id)
 
       render json: {
-        status: { code: 200, message: 'Signed in successfully.' },
+        status: { code: 200, message: "Signed in successfully." },
         data: {
           user: {
             id: user.id,
@@ -41,21 +39,21 @@ class Api::V1::Users::Auth::SessionsController < Devise::SessionsController
       }, status: :ok
     else
       render json: {
-        status: { code: 401, message: 'Invalid email/phone number or password.' }
+        status: { code: 401, message: "Invalid email/phone number or password." }
       }, status: :unauthorized
     end
   end
 
   def destroy
-    token = request.headers['Authorization']&.split(' ')&.last
+    token = request.headers["Authorization"]&.split(" ")&.last
 
     if token.present?
       render json: {
-        status: { code: 200, message: 'Signed out successfully.' }
+        status: { code: 200, message: "Signed out successfully." }
       }, status: :ok
     else
       render json: {
-        status: { code: 401, message: 'No token found or already signed out.' }
+        status: { code: 401, message: "No token found or already signed out." }
       }, status: :unauthorized
     end
   end
@@ -63,7 +61,6 @@ class Api::V1::Users::Auth::SessionsController < Devise::SessionsController
   private
 
   def sign_in_params
-    params.permit(user: [:email, :phone_number, :password, :role])
+    params.permit(user: [ :email, :phone_number, :password, :role ])
   end
-
 end
