b11a9cbc1fa9c2c20c1aedec15282a6ed4a15d5d
Haroon Siddique <haroonsiddique97@gmail.com>
2025-12-11 14:25:08 +0500

    updated serilizers based on frontend requirements

--- app/controllers/concerns/api_authentication.rb
+++ app/controllers/concerns/api_authentication.rb
@@ -1,16 +1,10 @@
 module ApiAuthentication
     extend ActiveSupport::Concern
-  
-    included do
-     puts '123'
-    end
-    
+
     private
-    
+
     def authenticate_user_from_token!
-      token = request.headers['Authorization']&.split(' ')&.last
-
-
+      token = request.headers["Authorization"]&.split(" ")&.last
 
       if token
         begin
@@ -18,25 +12,24 @@ module ApiAuthentication
 
         rescue => e
 
-          render json: { error: 'Invalid or expired token' }, status: :unauthorized and return
+          render json: { error: "Invalid or expired token" }, status: :unauthorized and return
         end
         if decoded
 
           @current_user = User.find_by(id: decoded[:user_id])
 
-          
           unless @current_user
-            render json: { error: 'User not found' }, status: :unauthorized
+            render json: { error: "User not found" }, status: :unauthorized
           end
         else
-          render json: { error: 'Invalid or expired token' }, status: :unauthorized
+          render json: { error: "Invalid or expired token" }, status: :unauthorized
         end
       else
-        render json: { error: 'No token provided' }, status: :unauthorized
+        render json: { error: "No token provided" }, status: :unauthorized
       end
     end
-    
+
     def current_user
       @current_user
     end
-  end
+end
--- app/serializers/ServiceSerilalizers/SubServiceShowSerializer.rb
+++ app/serializers/ServiceSerilalizers/SubServiceShowSerializer.rb
@@ -1,38 +1,44 @@
 module ServiceSerilalizers
-    class SubServiceShowSerializer < ActiveModel::Serializer
-      include Rails.application.routes.url_helpers
+  class SubServiceShowSerializer < ActiveModel::Serializer
+    include Rails.application.routes.url_helpers
 
-      attributes :id, :service_name, :sub_service_name, :description,
-                 :price, :price_bargain, :active_status,
-                 :created_at, :updated_at, :cover_image_url,
-                 :vendor_profile , :address
+    attributes :id, :service_id, :service_name, :sub_service_name, :description,
+               :price, :price_bargain, :active_status,
+               :cover_image_url,
+               :vendor_profile, :address
 
-      def address
-        return nil unless object.address.present?
+    def address
+      return nil unless object.address.present?
 
-        {
-          city: object.address.city,
-          longitude: object.address.longitude,
-          latitude: object.address.latitude,
-          address: object.address.address
-        }
-      end
+      {
+        city: object.address.city,
+        longitude: object.address.longitude,
+        latitude: object.address.latitude,
+        address: object.address.address,
+      }
+    end
 
-      def service_name
-        object.service.try(:service_name) 
-      end
+    def service_name
+      object.service.try(:service_name)
+    end
 
-      def vendor_profile
-        {
-          id: object.vendor_profile.id,
-          full_name: object.vendor_profile.full_name,
-          phone_number: object.vendor_profile.phone_number,
-        }
-      end
+    def vendor_profile
+      {
+        id: object.vendor_profile.id,
+        full_name: object.vendor_profile.full_name,
+        phone_number: object.vendor_profile.phone_number,
+        profile_image: profile_image_url(object.vendor_profile)
+      }
+    end
 
-      def cover_image_url
-        return nil unless object.sub_service_image.attached?
-        rails_blob_url(object.sub_service_image, only_path: false)
-      end
+    def profile_image_url(vendor_profile)
+      return nil unless vendor_profile.profile_image.attached?
+      rails_blob_url(vendor_profile.profile_image, only_path: false)
+    end
+
+    def cover_image_url
+      return nil unless object.sub_service_image.attached?
+      rails_blob_url(object.sub_service_image, only_path: false)
     end
   end
+end
